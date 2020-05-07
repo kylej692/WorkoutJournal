8a056e60d3e7b878cbd2205643f1d501952788f5
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-05-07 17:01:34 -0400

    Create Modal for editing logs

--- components/LogEntry.js
+++ components/LogEntry.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, View, TouchableOpacity } from 'react-native';
 import { Header, Content, Card, CardItem, Text, Body } from 'native-base';
+import Modal from 'react-native-modal';
+import ModifyLog from '../components/ModifyLog';
 
 const LogEntry = ({ item, deleteItem }) => {
     var count = 0;
+
+    const [isModalVisible, setModalVisible] = useState(true);
+  
+    const toggleModal = () => {
+      setModalVisible(!isModalVisible);
+    };
+
     return (
             <Content padder>
                 <Header style={{ height: 30, alignItems: "center", flexDirection: "row", backgroundColor: "#2C95FF" }}>
@@ -14,25 +24,34 @@ const LogEntry = ({ item, deleteItem }) => {
                     return (
                         <Card key={workout.id}>
                             <CardItem header bordered style={{ height: 45, borderBottomWidth: 1, borderColor: "black" }}>
-                                <Text style={{ color:"black", fontWeight: "bold", fontSize: 19 }}>{workout.name}</Text>
+                                <TouchableOpacity onPress={toggleModal}>
+                                    <Text style={{ color:"black", fontWeight: "bold", fontSize: 19 }}>{workout.name}</Text>
+                                </TouchableOpacity>
                             </CardItem>
                             <CardItem bordered style={{ height: 40, borderBottomWidth: 1, borderColor: "black" }}>
                                 <Text style={{ color:"black" }}>{"Set"}</Text>
                                 <Text style={{ color:"black", marginLeft: 130 }}>{"Reps"}</Text>
                                 <Text style={{ color:"black", marginLeft: "auto" }}>{"Weight"}</Text>
                             </CardItem>                            
-                                {workout.sets.map((set) => {
-                                    count += 1;
-                                    return (
-                                    <CardItem bordered style={{ height: 40 }} key={set.id}>
-                                        <Body style={{ flexDirection:"row" }}>
-                                            <Text>{count}</Text>
-                                            <Text style={{ marginLeft: "auto" }}>{set.reps}</Text>
-                                            <Text style={{ marginLeft: "auto" }}>{set.weight}</Text>
-                                        </Body>
-                                    </CardItem>
-                                    )})
-                                }
+                            {workout.sets.map((set) => {
+                                count += 1;
+                                return (
+                                <CardItem bordered style={{ height: 40 }} key={set.id}>
+                                    <Body style={{ flexDirection:"row" }}>
+                                        <Text>{count}</Text>
+                                        <Text style={{ marginLeft: "auto" }}>{set.reps}</Text>
+                                        <Text style={{ marginLeft: "auto" }}>{set.weight}</Text>
+                                    </Body>
+                                </CardItem>
+                                )})
+                            }
+                            <Modal isVisible={ isModalVisible } style={{ backgroundColor: "white", height: 50, flex: 1, alignItems: "center", padding: 100 }}>
+                                <View style={{flex: 1}}>
+                                    <ModifyLog />
+                                    <Button title="Delete" onPress={toggleModal}/>
+                                    <Button title="Cancel" onPress={toggleModal}/>
+                                </View>
+                            </Modal>  
                         </Card>
                         )
                     })
---
+++ components/ModifyLog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import { Content } from 'native-base';
+
+const ModifyLog = () => {
+    return (
+            <Content padder>
+                <Text>{"hi!"}</Text>
+            </Content>
+    );
+}
+
+export default ModifyLog;
