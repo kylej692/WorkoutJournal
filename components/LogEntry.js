59d6b47cfc11a6461aa26d2f0fd3ba4c74b8cb0c
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-05-09 17:56:18 -0400

    Display workout info in modal

--- components/LogEntry.js
+++ components/LogEntry.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Header, Content, Card, CardItem, Text, Body } from 'native-base';
 import Modal from 'react-native-modal';
 import ModifyLog from '../components/ModifyLog';
+import Icon from 'react-native-vector-icons/dist/AntDesign';
 
 const LogEntry = ({ item, deleteItem }) => {
     var count = 0;
@@ -17,45 +18,46 @@ const LogEntry = ({ item, deleteItem }) => {
 
     return (
             <Content padder>
-                <Header style={{ height: 30, alignItems: "center", flexDirection: "row", backgroundColor: "#2C95FF" }}>
-                    <Text style={{ fontSize: 17, marginRight: "auto", color: "white" }}>{item.time.date + "    "}</Text>
-                    <Text style={{ fontSize: 13, color: "white" }}>{item.time.start + "-" + item.time.end}</Text>
+                <Header style={styles.dateHeader}>
+                    <Text style={styles.dateText}>{item.time.date + "    "}</Text>
+                    <Text style={styles.timeText}>{item.time.start + "-" + item.time.end}</Text>
                 </Header>
                 {item.workouts.map((workout) => {
                     count = 0;
                     return (
                         <Card key={workout.id}>
-                            <CardItem header bordered style={{ height: 45, borderBottomWidth: 1, borderColor: "black" }}>  
+                            <CardItem header bordered style={styles.cardHeader}>  
                                 <TouchableOpacity onPress={() => {
                                     toggleModal(workout);
                                 }}>
-                                    <Text style={{ color:"black", fontWeight: "bold", fontSize: 19 }}>{workout.name}</Text>
+                                    <Text style={styles.workoutName}>{workout.name}</Text>
                                 </TouchableOpacity>
                             </CardItem>
-                            <CardItem bordered style={{ height: 40, borderBottomWidth: 1, borderColor: "black" }}>
-                                <Text style={{ color:"black" }}>{"Set"}</Text>
-                                <Text style={{ color:"black", marginLeft: 130 }}>{"Reps"}</Text>
-                                <Text style={{ color:"black", marginLeft: "auto" }}>{"Weight"}</Text>
+                            <CardItem bordered style={styles.cardLabelContainer}>
+                                <Text>{"Set"}</Text>
+                                <Text style={styles.cardText}>{"Reps"}</Text>
+                                <Text style={styles.cardText}>{"Weight"}</Text>
                             </CardItem>                            
                             {workout.sets.map((set) => {
                                 count += 1;
                                 return (
-                                <CardItem bordered style={{ height: 40 }} key={set.id}>
-                                    <Body style={{ flexDirection:"row" }}>
+                                <CardItem bordered style={styles.cardInfoContainer} key={set.id}>
+                                    <Body style={styles.cardBody}>
                                         <Text>{count}</Text>
-                                        <Text style={{ marginLeft: "auto" }}>{set.reps}</Text>
-                                        <Text style={{ marginLeft: "auto" }}>{set.weight}</Text>
+                                        <Text style={styles.cardText}>{set.reps}</Text>
+                                        <Text style={styles.cardText}>{set.weight}</Text>
                                     </Body>
                                 </CardItem>
                                 )})
                             }
-                            <Modal isVisible={ isModalVisible } style={{ backgroundColor: "white", height: 50, alignItems: "center" }}>
+                            <Modal scrollOffset={0} onRequestClose={() => {setModalVisible(!isModalVisible)}} isVisible={ isModalVisible } style={styles.modal}>
                                 <ModifyLog workout={selectedWorkout} />
-                                <TouchableOpacity style={{ padding: 10 }} onPress={() => {
-                                    deleteItem(selectedWorkout.id)}}>
-                                    <Text style={{ color: "red" }}>Delete</Text>
+                                <TouchableOpacity style={styles.deleteBtn} onPress={() => {
+                                    deleteItem(selectedWorkout.id), 
+                                    setModalVisible(!isModalVisible)}}>
+                                    <Icon style={styles.deleteIcon} name="delete" size={20} />
                                 </TouchableOpacity>
-                                <TouchableOpacity style={{ padding: 10 }} onPress={toggleModal}><Text>Cancel</Text></TouchableOpacity>
+                                <TouchableOpacity style={styles.cancelBtn} onPress={() => {setModalVisible(!isModalVisible)}}><Text>Cancel</Text></TouchableOpacity>
                             </Modal>
                         </Card>
                         )
@@ -64,5 +66,65 @@ const LogEntry = ({ item, deleteItem }) => {
             </Content>
     );
 }
+
+const styles = StyleSheet.create({
+    dateText: { 
+        fontSize: 17, 
+        marginRight: "auto", 
+        color: "white" 
+    },
+    timeText: { 
+        fontSize: 13, 
+        color: "white" 
+    },
+    dateHeader: {
+        height: 30, 
+        alignItems: "center", 
+        flexDirection: "row", 
+        backgroundColor: "#2C95FF"
+    },
+    cardHeader: {
+        height: 45, 
+        borderBottomWidth: 1, 
+        borderColor: "black"
+    },
+    workoutName: {
+        color:"black", 
+        fontWeight: "bold", 
+        fontSize: 19
+    },
+    cardLabel: { 
+        height: 40, 
+        borderBottomWidth: 1, 
+        borderColor: "black" 
+    },
+    cardText: {
+        marginLeft: "auto",
+    },
+    cardBody: { 
+        flexDirection:"row" 
+    },
+    cardInfoContainer: { 
+        height: 40 
+    },
+    modal: { 
+        backgroundColor: "white", 
+        height: 50, 
+        alignItems: "center"
+    },
+    deleteBtn: { 
+        position: 'absolute', 
+        bottom: 10, 
+        right: 10 
+    },
+    deleteIcon: { 
+        color: "red" 
+    },
+    cancelBtn: { 
+        position: 'absolute', 
+        bottom: 10, 
+        left: 10 
+    }
+  });
 
 export default LogEntry;
--- components/ModifyLog.js
+++ components/ModifyLog.js
@@ -1,13 +1,110 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
-import { Content } from 'native-base';
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Content, Text, Body, Card, CardItem } from 'native-base';
+import Header from '../components/Header';
+import Icon from 'react-native-vector-icons/dist/AntDesign';
 
 const ModifyLog = ({ workout }) => {
+    const [text, setText] = useState('');
+
+    const onChange = (textValue) => setText(textValue);
+
+    var count = 0;
+
     return (
-            <Content style={{ width: 370, backgroundColor: "grey" }}>
-                <Text>{workout.name}</Text>
+        <View>
+            <Header title="Edit Log" />
+            <Content padder>
+                <View style={styles.nameView}>                  
+                    <Text style={styles.name}>Name</Text>
+                    <TextInput defaultValue={workout.name} style={styles.nameInput} onChangeText={onChange} />
+                </View>
+                <View style={styles.infoView}>
+                {workout.sets.map((set) => {
+                    count += 1;
+                    return (
+                        <View style={styles.setView} key={set.id}>
+                            <Text style={styles.headerText}>{"Set " + count + ":"}</Text>
+                            <Text style={styles.infoText}>{"Reps "}</Text>
+                            <TextInput defaultValue={set.reps.toString()} style={styles.infoInput} onChangeText={onChange} />
+                            <Text style={styles.infoText}>{"Weight "}</Text>
+                            <TextInput defaultValue={set.weight.toString()} style={styles.infoInput} onChangeText={onChange} />        
+                        </View>
+                    )})
+                }
+                </View>
             </Content>
+            <TouchableOpacity style={styles.modifyBtn} onPress={() => addItem(text)}>
+                <Icon color="white" name="check" size={20} /><Text style={styles.finishText}>Finish</Text>
+            </TouchableOpacity> 
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    nameView: {
+        flexWrap: "wrap",
+        width: 370,
+        flexDirection: "row",
+    },
+    infoView: {
+        flex: 1,
+        marginBottom: 100,
+        marginTop: 50,
+    },
+    name: {
+        fontSize: 20,
+        padding: 5,
+        paddingRight: 10,
+        marginTop: 40,
+        marginLeft: 10
+    },
+    nameInput: {
+        backgroundColor: "#D9D9D9",
+        height: 35,
+        width: 250,
+        padding: 8,
+        fontSize: 16,
+        marginTop: 43,
+        borderRadius: 8 
+    },
+    infoInput: {
+        backgroundColor: "#D9D9D9",
+        height: 35,
+        width: 50,
+        borderRadius: 8,
+        margin: 10
+    },
+    modifyBtn: {
+        alignSelf: "center",
+        backgroundColor: "#2C95FF",
+        height: 35,
+        width: 90,
+        padding: 5,
+        marginBottom: 10,
+        marginLeft: 20,
+        right: 10,
+        borderRadius: 8,
+        flexWrap: "wrap",
+        flexDirection: "row" 
+    },
+    setView: {
+        marginBottom: 40,
+        flex: 1,
+        flexDirection: "row"
+    },
+    headerText: {
+        fontSize: 20,
+        padding: 10
+    },
+    infoText: {
+        fontSize: 15,
+        padding: 10
+    },
+    finishText: {
+        color: "white",
+        marginLeft: 5
+    }
+});
 
 export default ModifyLog;
