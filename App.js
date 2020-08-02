03062b915e68105d78a2b06cd913e14fcd223a2b
Derek-s-Jang <drkjng54@gmail.com>
2020-08-02 16:22:22 -0400

    Add TimerScreen tab

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/dist/AntDesign';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './components/HomeScreen';
-import ProgressScreen from './components/ProgressScreen'
+import ProgressScreen from './components/ProgressScreen';
+import TimerScreen from './components/TimerScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,24 +18,25 @@ const App = () => {
             let iconName;
 
             if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
+              iconName = focused ? 'home' : 'home';
             } else if (route.name === 'Progress') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
+              iconName = focused ? 'exclamationcircleo' : 'exclamationcircleo';
+            } else if (route.name === 'Timer') {
+              iconName = focused ? 'clockcircleo' : 'clockcircleo';
             }
 
             // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
+            return <Icon name={iconName} size={size} color={color} />;
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'tomato',
+          activeTintColor: '#2C95FF',
           inactiveTintColor: 'gray',
         }}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Progress" component={ProgressScreen} />
+        <Tab.Screen name="Timer" component={TimerScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
