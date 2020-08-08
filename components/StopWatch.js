a75cc4313b564574ec09d0033c64e4e392030077
Derek-s-Jang <drkjng54@gmail.com>
2020-08-08 16:21:18 -0400

    Add Timer and StopWatch components

---
+++ components/StopWatch.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+const StopWatch = () => {
+
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return (
+      <View>
+        <Text>{seconds}</Text>
+        <View>
+          <TouchableOpacity onPress={toggle}>
+            <Text>{isActive ? 'Stop' : 'Start'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={reset}>
+            <Text>Reset</Text> 
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+};
+
+export default StopWatch;
--- components/TimeScreen.js
+++ components/TimeScreen.js
@@ -1,43 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import StopWatch from './StopWatch';
+import Timer from './Timer';
 
 const TimeScreen = () => {
-
-  const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
-
-  function toggle() {
-    setIsActive(!isActive);
-  }
-
-  function reset() {
-    setSeconds(0);
-    setIsActive(false);
-  }
-
-  useEffect(() => {
-    let interval = null;
-    if (isActive) {
-      interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
-
   return (
       <View>
-        <Text>{seconds}</Text>
-        <View>
-          <TouchableOpacity onPress={toggle}>
-            <Text>{isActive ? 'Pause' : 'Start'}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={reset}>
-            <Text>Reset</Text> 
-          </TouchableOpacity>
-        </View>
+        <StopWatch/>
+        <Timer/>
       </View>
     );
 };
