259b8e2aa6ddfbfd5ed81e7e634d8b49393c039f
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-08-10 12:17:54 -0400

    Change LogEntry to a PureComponent

--- components/LogEntry.js
+++ components/LogEntry.js
@@ -1,59 +1,65 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Header, Card, CardItem, Text } from 'native-base';
 
-const LogEntry = ({ item, toggleInfoModal, toggleDateModal }) => {
-    var count = 0;
+class LogEntry extends PureComponent {
 
-    return (
-        <View>
-            <TouchableOpacity style={styles.dateTouchableOpacity} onPress={() => {
-                toggleDateModal(item);
-            }}>
-                <Header style={styles.dateHeader}>
-                    <Text style={styles.dateText}>{item.time.date + "    "}</Text>
-                    <Text style={styles.timeText}>{item.time.start + "-" + item.time.end}</Text>
-                </Header>
-            </TouchableOpacity>
-            {item.workouts.map((workout) => {
-                return (
-                    <Card key={workout.id} style={styles.card}>
-                        <CardItem header bordered style={styles.cardHeader}>  
-                            <TouchableOpacity onPress={() => {
-                                toggleInfoModal(workout);
-                            }}>
-                                <Text style={styles.workoutName}>{workout.name}</Text>
-                            </TouchableOpacity>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Text>Set</Text>
-                            <Text style={styles.cardText}>Reps</Text>
-                            <Text style={styles.cardText}>Wt (lbs)</Text>
-                        </CardItem>                           
-                        {workout.sets.map((set) => {
-                            count += 1;
-                            return (
-                            <CardItem bordered style={styles.cardInfoContainer} key={set.id}>
-                                <View style={styles.cardItemBody}>
-                                    <View style={styles.itemView}>
-                                        <Text>{set.num}</Text>
-                                    </View>
-                                    <View style={styles.itemView}>
-                                        <Text style={styles.repsNum}>{set.reps}</Text>
-                                    </View>
-                                    <View style={styles.itemView}>
-                                        <Text style={styles.weightNum}>{set.weight}</Text>
-                                    </View>   
-                                </View>                                  
+    constructor(props) {
+        super(props);
+        this.count = 0;
+    };
+
+    render() {
+        return (
+            <View>
+                <TouchableOpacity style={styles.dateTouchableOpacity} onPress={() => {
+                    this.props.toggleDateModal(this.props.item);
+                }}>
+                    <Header style={styles.dateHeader}>
+                        <Text style={styles.dateText}>{this.props.item.time.date + "    "}</Text>
+                        <Text style={styles.timeText}>{this.props.item.time.start + "-" + this.props.item.time.end}</Text>
+                    </Header>
+                </TouchableOpacity>
+                {this.props.item.workouts.map((workout) => {
+                    return (
+                        <Card key={workout.id} style={styles.card}>
+                            <CardItem header bordered style={styles.cardHeader}>  
+                                <TouchableOpacity onPress={() => {
+                                    this.props.toggleInfoModal(workout);
+                                }}>
+                                    <Text style={styles.workoutName}>{workout.name}</Text>
+                                </TouchableOpacity>
                             </CardItem>
-                            )})
-                        }
-                    </Card>
-                    )
-                })
-            }
-        </View>
-    );
+                            <CardItem bordered>
+                                <Text>Set</Text>
+                                <Text style={styles.cardText}>Reps</Text>
+                                <Text style={styles.cardText}>Wt (lbs)</Text>
+                            </CardItem>                           
+                            {workout.sets.map((set) => {
+                                this.count += 1;
+                                return (
+                                <CardItem bordered style={styles.cardInfoContainer} key={set.id}>
+                                    <View style={styles.cardItemBody}>
+                                        <View style={styles.itemView}>
+                                            <Text>{set.num}</Text>
+                                        </View>
+                                        <View style={styles.itemView}>
+                                            <Text style={styles.repsNum}>{set.reps}</Text>
+                                        </View>
+                                        <View style={styles.itemView}>
+                                            <Text style={styles.weightNum}>{set.weight}</Text>
+                                        </View>   
+                                    </View>                                  
+                                </CardItem>
+                                )})
+                            }
+                        </Card>
+                        )
+                    })
+                }
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
