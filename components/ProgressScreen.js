1593e0db86e158178aace3873c8913a30cf30fa3
Derek-s-Jang <drkjng54@gmail.com>
2020-09-20 16:42:48 -0400

    Add drop down menu

--- components/ProgressScreen.js
+++ components/ProgressScreen.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput } from 'react-native';
+import Header from './Header';
 import { db } from '../Database.js';
+import { Picker } from '@react-native-community/picker';
 
 const ProgressScreen = () => {
 
   const [myTextInput, setMyTextInput] = useState(React.createRef());
+  const [mode, setMode] = useState("Progressive Overload");
 
   const findExercise = (textValue) => {
     db.find({ "workouts": {$elemMatch:{"name": textValue}} }, function (err, docs) {
@@ -14,14 +17,24 @@ const ProgressScreen = () => {
   };
 
   return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1 }}>
+        <Header title='Progress Tracker'/>
+        <Picker
+          mode={"dropdown"}
+          selectedValue={mode}
+          onValueChange={(itemValue, itemIndex) => 
+            setMode(itemValue)
+          }>
+          <Picker.Item label="Progressive Overload" value="Progressive Overload"/>
+          <Picker.Item label="Repetitions" value="Repetitions"/>
+        </Picker>
         <TextInput 
           ref={myTextInput}
           placeholder="Enter Exercise Name" 
           onSubmitEditing={ (event) => {
             findExercise(event.nativeEvent.text)
             myTextInput.current.clear()
-          }}/>
+        }}/>
       </View>
     );
 };
