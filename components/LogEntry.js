fac03a3295b659be3b4ac2bade24fc7dc0dcbee0
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-09-20 16:31:18 -0400

    Create component for displaying workout list

--- components/LogEntry.js
+++ components/LogEntry.js
@@ -1,70 +1,30 @@
-import React, { PureComponent } from 'react';
-import { View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
-import { Header, Card, CardItem, Text } from 'native-base';
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Header, Text } from 'native-base';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import WorkoutListDisplay from './WorkoutListDisplay';
 
-class LogEntry extends PureComponent {
+const LogEntry = ({ item, toggleInfoModal, toggleDateModal, unitSystem }) => {
 
-    constructor(props) {
-        super(props);
-        this.count = 0;
-    };
-
-    render() {
-        return (
-            <View>
-                <TouchableOpacity style={styles.dateTouchableOpacity} onPress={() => {
-                    this.props.toggleDateModal(this.props.item);
-                }}>
-                    <Header style={styles.dateHeader}>
-                        <Icon style={{ color: "white", paddingRight: 10 }} name="clock-o" size={18}/>
-                        <Text style={styles.timeText}>{this.props.item.time.start + "-" + this.props.item.time.end}</Text>
-                    </Header>
-                </TouchableOpacity>
-                <FlatList 
-                    data={this.props.item.workouts}
-                    renderItem={(workoutData) => (
-                        <Card key={workoutData.item.id} style={styles.card}>
-                            <CardItem header bordered style={styles.cardHeader}>  
-                                <TouchableOpacity onPress={() => {
-                                    this.props.toggleInfoModal(this.props.item.id, workoutData.item);
-                                }}>
-                                    <Text style={styles.workoutName}>{workoutData.item.name}</Text>
-                                </TouchableOpacity>
-                            </CardItem>
-                            <CardItem bordered>
-                                <Text>Set</Text>
-                                <Text style={styles.cardText}>Reps</Text>
-                                {this.props.unitSystem == "Imperial" && <Text style={styles.cardText}>Wt (lbs)</Text>}
-                                {this.props.unitSystem == "Metric" && <Text style={styles.cardText}>Wt (kgs)</Text>}
-                            </CardItem>  
-                            <FlatList 
-                                data={workoutData.item.sets}
-                                renderItem={(setData) => {
-                                    this.count += 1;
-                                    return (
-                                        <CardItem bordered style={styles.cardInfoContainer} key={setData.item.id}>
-                                            <View style={styles.cardItemBody}>
-                                                <View style={styles.itemView}>
-                                                    <Text>{setData.item.num}</Text>
-                                                </View>
-                                                <View style={styles.itemView}>
-                                                    <Text style={styles.repsNum}>{setData.item.reps}</Text>
-                                                </View>
-                                                <View style={styles.itemView}>
-                                                    <Text style={styles.weightNum}>{setData.item.weight}</Text>
-                                                </View>   
-                                            </View>                                  
-                                        </CardItem>
-                                    )
-                                }}
-                            />                         
-                        </Card>
-                    )}
-                />
-            </View>
-        );
-    }
+    return (
+        <View>
+            <TouchableOpacity style={styles.dateTouchableOpacity} onPress={() => {
+                toggleDateModal(item);
+            }}>
+                <Header style={styles.dateHeader}>
+                    <Icon style={{ color: "white", paddingRight: 10 }} name="clock-o" size={18}/>
+                    <Text style={styles.timeText}>{item.time.start + "-" + item.time.end}</Text>
+                </Header>
+            </TouchableOpacity>
+            <WorkoutListDisplay
+                item={item} 
+                toggleInfoModal={toggleInfoModal}
+                toggleDateModal={toggleDateModal}
+                unitSystem={unitSystem}
+                key={item.id} 
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -80,46 +40,7 @@ const styles = StyleSheet.create({
         height: 30, 
         alignItems: "center", 
         backgroundColor: "#2C95FF"
-    },
-    cardHeader: {
-        height: 45, 
-        borderBottomWidth: 1, 
-        borderColor: "black"
-    },
-    workoutName: {
-        color:"black", 
-        fontWeight: "bold", 
-        fontSize: 19
-    },
-    cardItemBody: {
-        flex: 1, 
-        alignSelf: "stretch", 
-        flexDirection: "row"
-    },
-    itemView: { 
-        flex: 1, 
-        alignSelf: "stretch"
-    },
-    cardLabel: { 
-        height: 40, 
-        borderBottomWidth: 1, 
-        borderColor: "black" 
-    },
-    cardText: {
-        marginLeft: "auto"
-    },
-    repsNum: {
-        marginLeft: 30
-    },
-    weightNum: {
-        marginLeft: 70
-    },
-    weightNumType: {
-        fontSize: 15
-    },
-    cardInfoContainer: { 
-        height: 40 
-    },
+    }
   });
 
 export default LogEntry;
