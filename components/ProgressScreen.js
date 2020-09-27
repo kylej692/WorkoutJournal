26720594f97781210fba37139dae79d297b68a66
Derek-s-Jang <drkjng54@gmail.com>
2020-09-27 16:54:33 -0400

    Sort exercise

--- components/ProgressScreen.js
+++ components/ProgressScreen.js
@@ -8,6 +8,60 @@ const ProgressScreen = () => {
 
   const [myTextInput, setMyTextInput] = useState(React.createRef());
   const [mode, setMode] = useState("Max Weight");
+  const monthsInYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const labels = [];
+
+  const sortItems = (items) => {
+    const sorted = [...items].sort((a, b) => {
+      const month1 = monthsInYear.indexOf(a.time.date.slice(0, 3)) + 1;
+      const day1 = a.time.date.slice(4, 6);
+      const year1 = a.time.date.slice(8, 12);
+      const month2 = monthsInYear.indexOf(b.time.date.slice(0, 3)) + 1;
+      const day2 = b.time.date.slice(4, 6);
+      const year2 = b.time.date.slice(8, 12);
+
+      var date1 = month1 + "/" + day1 + "/" + year1;
+      var date2 = month2 + "/" + day2 + "/" + year2; 
+
+      const timeStr1 = a.time.start;
+      const timeStr2 = b.time.start;
+
+      var hour1 = "";
+      var minute1 = "";
+      var meridiem1 = "";
+      if (timeStr1.length == 7) {
+        hour1 = timeStr1.slice(0, 2);
+        minute1 = timeStr1.slice(3, 5);
+        meridiem1 = timeStr1.slice(5, 7);
+      } else {
+        hour1 = timeStr1.slice(0, 1);
+        minute1 = timeStr1.slice(2, 4);
+        meridiem1 = timeStr1.slice(4, 6);
+      }
+
+      var hour2 = "";
+      var minute2 = "";
+      var meridiem2 = "";
+      if (timeStr2.length == 7) {
+        hour2 = timeStr2.slice(0, 2);
+        minute2 = timeStr2.slice(3, 5);
+        meridiem2 = timeStr2.slice(5, 7);
+      } else {
+        hour2 = timeStr2.slice(0, 1);
+        minute2 = timeStr2.slice(2, 4);
+        meridiem2 = timeStr2.slice(4, 6);
+      }
+
+      const time1 = hour1 + ":" + minute1 + ":" + "00 " + meridiem1;
+      const time2 = hour2 + ":" + minute2 + ":" + "00 " + meridiem2;
+
+      const aDate = new Date(date1 + " " + time1);
+      const bDate = new Date(date2 + " " + time2);
+
+      return aDate - bDate;
+    });
+    return sorted;
+  };
 
   const findExercise = (textValue, mode) => {
     //var newText = textValue.replace(/\s/g, '');
@@ -16,14 +70,17 @@ const ProgressScreen = () => {
     let setInfo = [];
 
     db.find({ "workouts": {$elemMatch:{"name": textValue}} }, function (err, docs) {
-      console.log(docs);
-
       if (docs.length == 0) {
         Alert.alert("Exercise not found");
       } else {
-        docs.forEach(function (item, index) {
+        //Sort the list of objects here
+        sortedDocs = sortItems(docs)
+        console.log(sortedDocs);
+        
+        sortedDocs.forEach(function (item, index) {
           let workout = item.workouts
-
+          labels.push(item.time.date);
+          console.log(labels);
           workout.forEach(function (item, index) {
             if (item.name === textValue) {
               workoutInfo.push(item);
